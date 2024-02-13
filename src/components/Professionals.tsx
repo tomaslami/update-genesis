8f44012e679b30426edcb49432777cae84ca1a32
tomaslami <tomaslamiguralnik@gmail.com>
2024-02-13 19:50:01 -0300

    Professionals changes

--- src/components/Professionals.tsx
+++ src/components/Professionals.tsx
@@ -6,11 +6,11 @@ import { CardProfessionalsElements } from '../helpers/data'
 const Professionals = () => {
 
   return (
-    <section id='profesionales' className='min-h-screen w-full flex justify-start items-start md:p-10 p-4 flex-col gap-10 bg-cover bg-no-repeat bg-center bg-[url(/FondoProfesionales_Mobile.png)] lg:bg-[url(/FondoProfesionales.svg)]'>
+    <section id='profesionales' className='min-h-screen w-full flex justify-start items-start md:p-10 p-4 flex-col gap-5 bg-cover bg-no-repeat bg-center bg-[url(/FondoProfesionales_Mobile.png)] lg:bg-[url(/FondoProfesionales.svg)]'>
       <div className='flex flex-col justify-center items-center w-full h-1/5  '>
         <h1 className='w-full text-center text-2xl text-[#002337] font-extrabold '>NUESTROS PROFESIONALES</h1>
       </div>
-      <div className='w-full h-full flex flex-col gap-24 items-end py-10' >
+      <div className='w-full h-full flex flex-col gap-24 items-end' >
         {CardProfessionalsElements.map(pro => (
           <div key={pro.id}
             className='w-full flex ' style={{ flexDirection: pro.id % 2 === 0 ? 'row-reverse' : 'row' }}>
