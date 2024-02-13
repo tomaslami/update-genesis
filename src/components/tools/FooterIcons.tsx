ebd79d6c4c8784356e84890aee5f7b83c7908cb6
tomaslami <tomaslamiguralnik@gmail.com>
2024-02-13 19:54:44 -0300

    footer icons changes

--- src/components/tools/FooterIcons.tsx
+++ src/components/tools/FooterIcons.tsx
@@ -3,16 +3,15 @@ import React from 'react'
 
 const FooterIcons = () => {
   return (
-    <div className='lg:w-2/3 w-full h-3/6 flex flex-col justify-center items-center gap-10'>
+    <div className='lg:w-2/3 w-full h-3/6 flex flex-col justify-center items-center gap-10 p-3'>
       {FooterElements.map(footer => (
-        <div className='w-full flex justify-center items-center'>
-
-          <ul key={footer.id} className='w-full flex justify-start items-center flex-row gap-4'>
+        <div className='w-full flex justify-center items-center '>
+          <ul key={footer.id} className='w-full flex justify-start items-center flex-row gap-4 '>
             <li>
               <img src={footer.icon} alt={footer.img_alt} className='text-white text-lg font-light' />
             </li>
             <li>
-              <h4 className='text-white text-base font-normal'>{footer.text}</h4>
+              <h4 className='text-white text-sm font-normal'>{footer.text}</h4>
             </li>
           </ul>
         </div>
