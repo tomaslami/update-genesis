6d3685d74528e0124912ca9e8e69cf3733a56eff
tomaslami <tomaslamiguralnik@gmail.com>
2024-02-17 00:23:50 -0300

    last changes

--- src/components/AboutUs.tsx
+++ src/components/AboutUs.tsx
@@ -13,7 +13,7 @@ const AboutUs = () => {
           <img src="/ABOUT-US.webp" alt="Ejercicios" className='lg:w-[500px] w-[400px] h-[200px] lg:h-[300px]' />
         </div>
         <div className='w-full lg:w-1/2 h-full flex justify-center items-center flex-col'>
-          <p className='w-11/12 font-bold text-justify text-base'>
+          <p className='w-11/12 font-bold text-center text-base'>
             Somos <span className='text-[#F28C38]'>GENESIS</span> es un centro especializado en la evaluación y tratamiento de patologias osteomusculares. Es atendido única y exclusivamente por profesionales licenciados en Kinesiología y Fisiatría cada uno especializado en su método y con una amplia carrera en la profesión habiendo ejercido en centros y hospitales de renombre tanto del ambito pubico como privado.
           </p>
         </div>
